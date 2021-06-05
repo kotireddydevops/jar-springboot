7a0e79cab9d551755192fd3a99859a28869963af
Kotireddy <73218741+kotireddydevops@users.noreply.github.com>
2021-06-05 21:01:05 +0530

    Update EmployeeController.java

--- src/main/java/com/restcontroller/EmployeeController.java
+++ src/main/java/com/restcontroller/EmployeeController.java
@@ -8,6 +8,12 @@ public class EmployeeController {
 
 	@RequestMapping("hello")
 	public String doSomething(){
-		return "Hello World!!! how are you kotireddy ph 9553144617";
+		              return "Hello World!!! how are you kotireddy ph 9553144617
+			
+	                                  <div class="col-md-4">
+                                              <h2>Docker</h2>
+                                              <p>Docker is a set of platform as a service products that uses OS-level virtualization to deliver software in packages called containers. Containers are isolated from one another and bundle their own software, libraries and configuration files; they can communicate with each other through well-defined channels</p>
+                                              <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+                                          </div>";
 	}
 }
